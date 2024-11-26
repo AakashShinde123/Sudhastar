f87035276a92f6f805b41ef48f62466c203f3d71
Aakash B. Shinde <113911171+AakashShinde123@users.noreply.github.com>
2024-11-26 15:22:20 +0530

    Update settings.py

--- foodzone/settings.py
+++ foodzone/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS = [ 
     STATIC_DIR,
