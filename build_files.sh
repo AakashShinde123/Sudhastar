13819903d2f50ec560a1c5d7375ee4626375e936
Aakash B. Shinde <113911171+AakashShinde123@users.noreply.github.com>
2024-11-26 13:27:42 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Ensure python3 is being used
-python3 --version  # Check python version
-pip3 install -r requirements.txt
+/usr/bin/python3 --version  # Check python version
+/usr/bin/pip3 install -r requirements.txt
 
 # Run Django collectstatic command to gather static files
-python3 manage.py collectstatic --noinput
-
-# Debug: Check if static files are collected
-echo "Static files collected:"
-ls static/
+/usr/bin/python3 manage.py collectstatic --noinput
 
 # Optionally, run migrations if needed
-python3 manage.py migrate --noinput
+/usr/bin/python3 manage.py migrate --noinput
 
 # Ensure staticfiles_build directory is created and populated
 mkdir -p staticfiles_build
@@ -21,5 +16,3 @@ cp -r static/* staticfiles_build/
 # Debug: Check if staticfiles_build is populated
 echo "Contents of staticfiles_build:"
 ls staticfiles_build/
-
-# Additional build steps as necessary
