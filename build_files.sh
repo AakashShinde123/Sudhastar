711596665d042690d19b4fed09c92979a881c861
Aakash B. Shinde <113911171+AakashShinde123@users.noreply.github.com>
2024-11-26 15:35:18 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python 3.12.6 pip install -r requirements.txt
-python 3.12.6 manage.py collectstatic --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
 echo "BUILD END"
