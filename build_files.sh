cded90a5edab366e51c5554fa2ac1a49026392dd
Aakash B. Shinde <113911171+AakashShinde123@users.noreply.github.com>
2024-11-26 15:32:21 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-
 echo "BUILD START"
-
-# Use Python 3.12 explicitly
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
-
-# Collect static files without interaction
-python3.12 manage.py collectstatic --noinput
-
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
 echo "BUILD END"
