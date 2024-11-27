d661057e8f112fdbd58ad0f2ede7c169ea07e6fe
Aakash B. Shinde <113911171+AakashShinde123@users.noreply.github.com>
2024-11-27 14:27:15 +0530

    Update settings.py

--- foodzone/settings.py
+++ foodzone/settings.py
@@ -23,13 +23,17 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+   MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+,
 ]
 
 ROOT_URLCONF = 'foodzone.urls'
@@ -73,12 +77,12 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+# Use WhiteNoise for static file compression and caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
