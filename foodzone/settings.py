10276714f1771e0681350f79e7fa3257ade2e914
Aakash B. Shinde <113911171+AakashShinde123@users.noreply.github.com>
2024-11-27 12:16:06 +0530

    Update settings.py

--- foodzone/settings.py
+++ foodzone/settings.py
@@ -57,14 +57,20 @@ WSGI_APPLICATION = 'foodzone.wsgi.application'
 # Database Configuration (use PostgreSQL for production)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
